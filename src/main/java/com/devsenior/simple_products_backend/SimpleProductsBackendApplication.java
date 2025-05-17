05398a74d1097ffca6eee8eabc8d2bec0d789692
Cesar Augusto Diaz Arriaga <black@utp.edu.co>
2025-05-17 15:47:50 -0500

    Initial commit

---
+++ src/main/java/com/devsenior/simple_products_backend/SimpleProductsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.devsenior.simple_products_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleProductsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleProductsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsenior/simple_products_backend/SimpleProductsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsenior.simple_products_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleProductsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
